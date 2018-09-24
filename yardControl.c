b5dc0457ba2cbdc092b352a3dcfe8dc6f999c6f9
Bodo Bauer <bb@bb-zone.com>
2018-09-24 22:49:49 +0200

    update

--- yardControl.c
+++ yardControl.c
@@ -19,6 +19,7 @@
 #include "pushButton.h"
 #include <wiringPi.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <pcf8574.h>
 
 /* ----------------------------------------------------------------------------------- *

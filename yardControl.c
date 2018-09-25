742cbb0476d98883a5848212df79dc07b5ce3777
Bodo Bauer <bb@bb-zone.com>
2018-09-25 23:34:58 +0200

    update

--- yardControl.c
+++ yardControl.c
@@ -179,7 +179,7 @@ bool readConfig(void) {
  * ----------------------------------------------------------------------------------- */
 void dumpSequence( int sequenceIdx ) {
     int step = 0;
-    sequence_t *seq = &sequence[sequenceIdx];
+    sequence_t *seq = sequence[sequenceIdx];
     while ( seq[step]->offset >= 0 ) {
 
         step++;

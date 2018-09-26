0635af9b2fa7d65dac31f40fa206786603274074
Bodo Bauer <bb@bb-zone.com>
2018-09-26 21:54:52 +0200

    update

--- readConfig.h
+++ readConfig.h
@@ -1,4 +1,4 @@
-/* *********************************************************************************** */
+* *********************************************************************************** */
 /*                                                                                     */
 /*  Copyright (c) 2018 by Bodo Bauer <bb@bb-zone.com>                                  */
 /*                                                                                     */
@@ -15,61 +15,47 @@
 /*  You should have received a copy of the GNU General Public License                  */
 /*  along with this program.  If not, see <http://www.gnu.org/licenses/>.              */
 /* *********************************************************************************** */
-#ifndef yardControl_h
-#define yardControl_h
+
+#ifndef readConfig_h
+#define readConfig_h
+
+#include <stdio.h>
 #include "pushButton.h"
 
+/* ----------------------------------------------------------------------------------- *
+ * A step in a sequence
+ * ----------------------------------------------------------------------------------- */
+typedef struct sequence_t {
+    int          offset;     // offset after sequence start this action shall be triggered
+    pushbutton_t *valve;     // valve to be switched
+    bool         state;      // new state of valve
+} sequence_t;
 
 /* ----------------------------------------------------------------------------------- *
- * PCF8574 IO extender
+ * Start time
  * ----------------------------------------------------------------------------------- */
-#define ADDR_IOEXT_0   0x38
-#define PINBASE_0        64
-
-#define ADDR_IOEXT_1   0x39
-#define PINBASE_1        72
+typedef struct starttime_t {
+    int tm_min;
+    int tm_hour;
+} starttime_t;
 
 /* ----------------------------------------------------------------------------------- *
- * output pins for valve control
+ * Some globals we can't do without
  * ----------------------------------------------------------------------------------- */
-#define VALVE_A       PINBASE_0+0  // control valve A
-#define VALVE_B       PINBASE_0+1  // control valve B
-#define VALVE_C       PINBASE_0+2  // control valve C
-#define VALVE_D       PINBASE_0+3  // control valve D
+extern char *configFile    = CONFIG_FILE;     /* configuration file                    */
+extern sequence_t  sequence[2][MAX_STEP];     /* two program sequences of max 40 steps */
+extern starttime_t startTime[2];              /* start times for each sequence         */
 
 /* ----------------------------------------------------------------------------------- *
- * Output pins for control LEDs
+ * Settings
  * ----------------------------------------------------------------------------------- */
-#define LED_S2        PINBASE_1+0  // sequence 2 selected
-#define LED_AUTO      PINBASE_1+1  // automatic mode enabled
-#define LED_S1        PINBASE_1+2  // sequence 1 selected
-#define LED_RUN       PINBASE_1+3  // selected sequence is running
+#define MAX_STEP        100  // max 100 steps per sequence (50 commands)
+#define TIME_SCALE        1  // unit scale fpr secuence, set to 60 to get minutes
 
 /* ----------------------------------------------------------------------------------- *
- * input pins for push buttons
+ * Prototypes
  * ----------------------------------------------------------------------------------- */
-#define BUTTON_A      PINBASE_0+4   // switch valve A
-#define BUTTON_B      PINBASE_0+5   // switch valve B
-#define BUTTON_C      PINBASE_0+6   // switch valve C
-#define BUTTON_D      PINBASE_0+7   // switch valve D
+bool readConfig (void);                  // read and parse config file
+void dumpSequence(int sequenceIdx);      // dump configuration in config file format
 
-#define NC_1_4        PINBASE_1+4   // not connected
-#define BUTTON_AUTO   PINBASE_1+5   // toggle automatic mode
-#define BUTTON_SELECT PINBASE_1+6   // select between sequence 1 and 2
-#define BUTTON_RUN    PINBASE_1+7   // run selected sequence
-
-/* ----------------------------------------------------------------------------------- *
- * radio groups for bush buttons
- * ----------------------------------------------------------------------------------- */
-#define RG_NONE       0
-#define RG_VALVES     1
-#define RG_SEQUENCES  2
-
-/* ----------------------------------------------------------------------------------- *
- * Default valued for configurable parameter
- * ----------------------------------------------------------------------------------- */
-#define DEBUG        0                           /* no debug info by default           */
-#define SEQUENCE     0                           /* run sequence 0 by default          */
-#define CONFIG_FILE  "/etc/yardcontrol.cfg"      /* read config from etc               */
-
-#endif /* yardControl_h */
+#endif /* readConfig_h */
--- yardControl.c
+++ yardControl.c
@@ -17,6 +17,7 @@
 /* *********************************************************************************** */
 #include "yardControl.h"
 #include "pushButton.h"
+#include "readConfig.h"
 #include <wiringPi.h>
 #include <pcf8574.h>
 
@@ -32,9 +33,6 @@
  * ----------------------------------------------------------------------------------- */
 int  debug          = DEBUG;                  /* debug level                           */
 int  activeSequence = SEQUENCE;               /* sequence to run                       */
-char *configFile    = CONFIG_FILE;            /* configuration file                    */
-sequence_t  sequence[2][MAX_STEP];            /* two program sequences of max 40 steps */
-starttime_t startTime[2];                     /* start times for each sequence         */
 
 /* ----------------------------------------------------------------------------------- *
  * System modes
@@ -47,8 +45,6 @@ enum Modes { MANUAL_MODE, SEQUENCE_MODE, AUTOMATIC_MODE } systemMode;
 void setup(void);
 int  main(int rgc, char *argv[]);
 void lockValveControl(bool on);
-bool readConfig (void);
-void dumpSequence(int sequenceIdx);
 
 // Bush button actions
 void setLed( pushbutton_t *button );
@@ -78,169 +74,7 @@ pushbutton_t pushButtons[] = {
     {'0', -1, -1, false, -1, false, -1},
 };
 
-/* ----------------------------------------------------------------------------------- *
- * Read config file
- * ----------------------------------------------------------------------------------- */
-char *nextValue( char **cursor) {
-    while (**cursor && **cursor != ' ') (*cursor)++;                   /*   skip token */
-    **cursor = '\0'; (*cursor)++;                                      /* end of token */
-    while (**cursor && **cursor == ' ') (*cursor)++;                   /* skip spaces  */
-    return *cursor;
-}
-
-bool readConfig(void) {
-    FILE *fp = NULL;
-    fp = fopen(configFile, "rb");
-    int sequenceIdx = -1, step = -1, offset=-1, lineNo=1;
-    
-    // start with two empty sequences
-    sequence[0][0].offset = -1;
-    startTime[0].tm_hour  = -1;
-    
-    sequence[1][0].offset = -1;
-    startTime[1].tm_hour  = -1;
-
-    if (fp) {
-        char  *line=NULL;
-        char  *cursor;
-        size_t n=0;
-        size_t length = getline(&line, &n, fp);
-        
-        while ( length != -1) {
-            if ( length > 1 ) {                              /* skip empty lines       */
-                cursor = line;
-                if ( line[length-1] == '\n' ) {             /* remove trailing newline */
-                    line[length-1] = '\0';
-                }
-                
-                while ( *cursor == ' ' || *cursor == '\t' ) cursor++;
-                
-                if ( *cursor != '#') {                          /* skip '#' comments   */
-                    char *token = cursor;
-                    char *value = nextValue(&cursor);
-                    
-                    if (!strcmp(token, "SEQUENCE")) {
-                        sequenceIdx++;
-                        step   = 0;
-                        offset = 0;
-                    } else if (!strcmp(token, "TIME")) {
-                        // expected format is hh:mm
-                        char *hh, *mm;
-                        int hour, min;
-                        hh=value;
-                        mm=value+3;
-                        *(value+2)='\0';
-                        *(value+5)='\0';
-                        hour=atoi(hh);
-                        min =atoi(mm);
-                        if( hour>=0 && hour<24 && min>=0 && min<60 ) {
-                            startTime[sequenceIdx].tm_min  = min;
-                            startTime[sequenceIdx].tm_hour = hour;
-                        } else {
-                            printf ( "[%s:%04d] ERROR: TIME expected as hh:mm\n", configFile, lineNo );
-                        }
-                    } else if (!strcmp(token, "PAUSE")) {
-                        int time  = atoi(value);
-                        if (time > 0 ) {
-                            offset+=(time*TIME_SCALE-1);
-                        } else {
-                            printf ( "[%s:%04d] ERROR: Wromg time in DELAY: %d\n", configFile, lineNo, time );
-                        }
-                    } else if (!strcmp(token, "VALVE")) {
-                        char *valve = cursor;
-                        int time  = atoi(nextValue(&cursor));
-                        int buttonIdx;
-                        if (time > 0 ) {
-                            buttonIdx = toupper(*valve) - 'A';  // A->0, B->1, C->2, D->3
-                            if ( buttonIdx >= 0 && buttonIdx <= 3 && step < (MAX_STEP-2)) {   // Add step to sequence
-                                
-                                // turn valve on
-                                sequence[sequenceIdx][step].offset = offset;
-                                sequence[sequenceIdx][step].valve  = &pushButtons[buttonIdx];
-                                sequence[sequenceIdx][step].state  = true;
-                                step++;
-                                offset += (time*TIME_SCALE);
-
-                                // turn valve off
-                                sequence[sequenceIdx][step].offset = offset;
-                                sequence[sequenceIdx][step].valve  = &pushButtons[buttonIdx];
-                                sequence[sequenceIdx][step].state  = false;
-                                step++;
-                                offset++;
-                                
-                                // Add End marker
-                                sequence[sequenceIdx][step].offset = -1;
-                            } else {
-                                if ( step >= (MAX_STEP-2) ) {
-                                    printf ( "[%s:%04d] ERROR: Sequence too long, ignoring line \n", configFile, lineNo );
-                                } else {
-                                    printf ( "[%s:%04d] ERROR: Unknown VALVE: %s\n", configFile, lineNo, valve );
-                                }
-                            }
-                        } else {
-                            printf ( "[%s:%04d] ERROR: Wromg time in VALVE: %d\n", configFile, lineNo, time );
-                        }
-                    } else {
-                        printf ( "[%s:%04d] WARNING: Skipping unknown command: %s\n", configFile, lineNo, token );
-                    }
-                }
-            }
-            free(line);
-            n=0;
-            length = getline(&line, &n, fp);
-            lineNo++;
-        }
-        fclose(fp);
-    }
-
-    dumpSequence( 0 );
-    dumpSequence( 1 );
-
-    return true;
-}
-
-/* ----------------------------------------------------------------------------------- *
- * Dump sequence definition
- * ----------------------------------------------------------------------------------- */
-void dumpSequence( int sequenceIdx ) {
-    int step = 0;
-    int lastON = 0, lastOFF = 0;
-    sequence_t *seq = sequence[sequenceIdx];
-
-    if ( seq[0].offset >=0 ) {
-        printf("# ----------------------------------------------------------------------------------- #\n");
-        printf("# Sequence %d                                                                          #\n", sequenceIdx );
-        printf("# ----------------------------------------------------------------------------------- #\n");
-        printf("SEQUENCE START\n");
-        if ( startTime[sequenceIdx].tm_hour >= 0 ) {
-            printf( "  START %02d:%02d\n", startTime[sequenceIdx].tm_hour, startTime[sequenceIdx].tm_min);
-        } else {
-            printf("#                     no START time defined, sequence will not run in automatic mode\n");
-        }
-        while ( seq[step].offset >= 0 ) {
-            if ( seq[step].state ) {
-                if ( seq[step].offset > (lastOFF+1) ) {
-                    printf("  PAUSE %d\n", (seq[step].offset-lastOFF)/TIME_SCALE );
-                }
-                lastON = seq[step].offset;
-            } else {
-                printf("  VALVE %c %d\n", seq[step].valve->name, (seq[step].offset-lastON)/TIME_SCALE );
-                lastOFF = seq[step].offset;
-            }
-            printf("#                     %03d t+%04d %c %s\n",
-                   step,
-                   seq[step].offset,
-                   seq[step].valve->name,
-                   seq[step].state? "ON":"OFF");
-            step++;
-        }
-    } else {
-        printf("# ----------------------------------------------------------------------------------- #\n");
-        printf("# Sequence %d not defined                                                             #\n", sequenceIdx );
-        printf("# ----------------------------------------------------------------------------------- #\n");
-    }
-}
-
+/
 /* ----------------------------------------------------------------------------------- *
  * Enable/Disable manual valve control (radio group: RG_VALVES)
  * ----------------------------------------------------------------------------------- */

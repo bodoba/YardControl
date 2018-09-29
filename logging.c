b3e88130b662b51cb7e44bf8efd40f50878aafdc
Bodo Bauer <bb@bb-zone.com>
2018-09-29 23:58:53 +0200

    update

--- logging.c
+++ logging.c
@@ -25,7 +25,7 @@
  * ----------------------------------------------------------------------------------- */
 static int  logLevel  = LOG_INFO;
 static bool useSyslog = true;
-static char logLevelText[5][] = {"FATAL", "ERROR", "WARNING", "INFO", "DEBUG"};
+static const char *logLevelText[5] = {"FATAL", "ERROR", "WARNING", "INFO", "DEBUG"};
 
 /* ----------------------------------------------------------------------------------- *
  * init logging

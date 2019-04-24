78dc03e3a094e132fe37f868c8ed7c65936831b0
Bodo Bauer <bb@bb-zone.com>
2019-04-24 23:28:55 +0200

    update

--- persistState.h
+++ persistState.h
@@ -14,8 +14,7 @@
 /*  You should have received a copy of the GNU General Public License                  */
 /*  along with this program.  If not, see <http://www.gnu.org/licenses/>.              */
 /* *********************************************************************************** */
-#include <ctype.h>
-
+#include <stdbool.h>
 
 #ifndef persistState_h
 #define persistState_h
